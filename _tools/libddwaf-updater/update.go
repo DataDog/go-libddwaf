9bc6e02ce679a72ed7a58ff43c91ae0804805c44
Romain Marcadier <romain@datadoghq.com>
2023-11-06 16:12:19 +0100

    add vendor.go gile to make tooling happy

--- _tools/libddwaf-updater/update.go
+++ _tools/libddwaf-updater/update.go
@@ -72,6 +72,9 @@ func main() {
 			created = true
 		}
 		if created || force {
+			if err := os.WriteFile(path.Join(embedDir, "vendor.go"), []byte(vedorMarkerFile), 0644); err != nil {
+				panic(err)
+			}
 			createEmbedSource(tgt)
 		}
 		go handleTarget(&wg, version, tgt, embedDir, assets)
@@ -278,6 +281,17 @@ var targets = []target{
 	// {os: "linux", arch: "armv7", ext: "so", assetLabel: "armv7-linux-musl"},
 	// {os: "linux", arch: "i386", ext: "so", assetLabel: "i386-linux-musl"},
 }
+
+const vedorMarkerFile = `// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package vendor
+
+// This file is there to make sure "go mod vendor" does not drop those directories, as it otherwise removes
+// any directiory that contains no ".go" files.
+`
 
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
---
+++ internal/vendor/windows-amd64/vendor.go
@@ -0,0 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package vendor
+
+// This file is there to make sure "go mod vendor" does not drop those directories, as it otherwise removes
+// any directiory that contains no ".go" files.
