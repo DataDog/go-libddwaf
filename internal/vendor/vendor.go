7a72925e4c0bff5243516a8f472f5f7cf98cd54c
Romain Marcadier <romain@datadoghq.com>
2023-11-06 16:30:13 +0100

    Typo fixes

--- internal/vendor/vendor.go
+++ internal/vendor/vendor.go
@@ -5,7 +5,6 @@
 
 //go:build ((darwin && (amd64 || arm64)) || (linux && (amd64 || arm64)) || (windows && (amd64 || 386))) && !go1.22
 
-// Package vendor provides a built-in WAF library version for the relevant runtime platform.
 package vendor
 
 import (
